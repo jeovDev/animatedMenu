8e27ec9bfc8b44acb41032a8d2a8bcb54eb80eaf
Jeovanne Lugo <jeovanneclugo@gmail.com>
2023-04-27 07:44:57 +0800

    first commit

---
+++ animatedMenu/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace animatedMenu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuLogout = new System.Windows.Forms.Label();
+            this.menuSettings = new System.Windows.Forms.Label();
+            this.menuHome = new System.Windows.Forms.Label();
+            this.menuAbout = new System.Windows.Forms.Label();
+            this.menuDashboard = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.metroControlBox1 = new ReaLTaiizor.Controls.MetroControlBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(48)))), ((int)(((byte)(78)))));
+            this.panel1.Controls.Add(this.menuLogout);
+            this.panel1.Controls.Add(this.menuSettings);
+            this.panel1.Controls.Add(this.menuHome);
+            this.panel1.Controls.Add(this.menuAbout);
+            this.panel1.Controls.Add(this.menuDashboard);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1028, 92);
+            this.panel1.TabIndex = 0;
+            // 
+            // menuLogout
+            // 
+            this.menuLogout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(48)))), ((int)(((byte)(78)))));
+            this.menuLogout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuLogout.ForeColor = System.Drawing.Color.White;
+            this.menuLogout.Image = ((System.Drawing.Image)(resources.GetObject("menuLogout.Image")));
+            this.menuLogout.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.menuLogout.Location = new System.Drawing.Point(315, 25);
+            this.menuLogout.Name = "menuLogout";
+            this.menuLogout.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.menuLogout.Size = new System.Drawing.Size(74, 60);
+            this.menuLogout.TabIndex = 0;
+            this.menuLogout.Text = "Logout";
+            this.menuLogout.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // menuSettings
+            // 
+            this.menuSettings.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(48)))), ((int)(((byte)(78)))));
+            this.menuSettings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuSettings.ForeColor = System.Drawing.Color.White;
+            this.menuSettings.Image = ((System.Drawing.Image)(resources.GetObject("menuSettings.Image")));
+            this.menuSettings.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.menuSettings.Location = new System.Drawing.Point(241, 25);
+            this.menuSettings.Name = "menuSettings";
+            this.menuSettings.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.menuSettings.Size = new System.Drawing.Size(74, 60);
+            this.menuSettings.TabIndex = 2;
+            this.menuSettings.Text = "Settings";
+            this.menuSettings.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // menuHome
+            // 
+            this.menuHome.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(48)))), ((int)(((byte)(78)))));
+            this.menuHome.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuHome.ForeColor = System.Drawing.Color.White;
+            this.menuHome.Image = ((System.Drawing.Image)(resources.GetObject("menuHome.Image")));
+            this.menuHome.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.menuHome.Location = new System.Drawing.Point(93, 25);
+            this.menuHome.Name = "menuHome";
+            this.menuHome.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.menuHome.Size = new System.Drawing.Size(74, 60);
+            this.menuHome.TabIndex = 4;
+            this.menuHome.Text = "Home";
+            this.menuHome.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // menuAbout
+            // 
+            this.menuAbout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(48)))), ((int)(((byte)(78)))));
+            this.menuAbout.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuAbout.ForeColor = System.Drawing.Color.White;
+            this.menuAbout.Image = ((System.Drawing.Image)(resources.GetObject("menuAbout.Image")));
+            this.menuAbout.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.menuAbout.Location = new System.Drawing.Point(167, 25);
+            this.menuAbout.Name = "menuAbout";
+            this.menuAbout.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.menuAbout.Size = new System.Drawing.Size(74, 60);
+            this.menuAbout.TabIndex = 1;
+            this.menuAbout.Text = "About";
+            this.menuAbout.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // menuDashboard
+            // 
+            this.menuDashboard.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(48)))), ((int)(((byte)(78)))));
+            this.menuDashboard.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menuDashboard.ForeColor = System.Drawing.Color.White;
+            this.menuDashboard.Image = ((System.Drawing.Image)(resources.GetObject("menuDashboard.Image")));
+            this.menuDashboard.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.menuDashboard.Location = new System.Drawing.Point(19, 25);
+            this.menuDashboard.Name = "menuDashboard";
+            this.menuDashboard.Padding = new System.Windows.Forms.Padding(0, 5, 0, 5);
+            this.menuDashboard.Size = new System.Drawing.Size(74, 60);
+            this.menuDashboard.TabIndex = 3;
+            this.menuDashboard.Text = "Dashboard";
+            this.menuDashboard.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(22, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Animated Menu ";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(32)))), ((int)(((byte)(52)))));
+            this.panel2.Controls.Add(this.metroControlBox1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1028, 36);
+            this.panel2.TabIndex = 11;
+            // 
+            // metroControlBox1
+            // 
+            this.metroControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.metroControlBox1.CloseHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(183)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.metroControlBox1.CloseHoverForeColor = System.Drawing.Color.White;
+            this.metroControlBox1.CloseNormalForeColor = System.Drawing.Color.Gray;
+            this.metroControlBox1.DefaultLocation = ReaLTaiizor.Controls.MetroControlBox.LocationType.Edge;
+            this.metroControlBox1.DisabledForeColor = System.Drawing.Color.DimGray;
+            this.metroControlBox1.Location = new System.Drawing.Point(928, 0);
+            this.metroControlBox1.MaximizeBox = true;
+            this.metroControlBox1.MaximizeHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.metroControlBox1.MaximizeHoverForeColor = System.Drawing.Color.Gray;
+            this.metroControlBox1.MaximizeNormalForeColor = System.Drawing.Color.Gray;
+            this.metroControlBox1.MetroStyleManager = null;
+            this.metroControlBox1.MinimizeBox = true;
+            this.metroControlBox1.MinimizeHoverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(238)))), ((int)(((byte)(238)))), ((int)(((byte)(238)))));
+            this.metroControlBox1.MinimizeHoverForeColor = System.Drawing.Color.Gray;
+            this.metroControlBox1.MinimizeNormalForeColor = System.Drawing.Color.Gray;
+            this.metroControlBox1.Name = "metroControlBox1";
+            this.metroControlBox1.Size = new System.Drawing.Size(100, 25);
+            this.metroControlBox1.Style = ReaLTaiizor.Enum.Metro.Style.Light;
+            this.metroControlBox1.TabIndex = 12;
+            this.metroControlBox1.Text = "metroControlBox1";
+            this.metroControlBox1.ThemeAuthor = "Taiizor";
+            this.metroControlBox1.ThemeName = "MetroLite";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1028, 592);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label menuLogout;
+        private System.Windows.Forms.Label menuSettings;
+        private System.Windows.Forms.Label menuHome;
+        private System.Windows.Forms.Label menuAbout;
+        private System.Windows.Forms.Label menuDashboard;
+        private System.Windows.Forms.Panel panel2;
+        private ReaLTaiizor.Controls.MetroControlBox metroControlBox1;
+    }
+}
+
---
+++ animatedMenu/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace animatedMenu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            List<Label> menu = new List<Label>() { menuDashboard, menuHome, menuAbout,menuSettings,menuLogout};
+            menuEventHandler(menu);
+        }
+
+        private void menuEventHandler(List<Label> Menu) {
+            foreach (var menu in Menu) {
+                
+                menu.MouseHover += Menu_MouseHover;
+                menu.MouseLeave += Menu_MouseLeave;
+            }
+        }
+
+        private void Menu_MouseLeave(object sender, EventArgs e)
+        {
+            ((Label)sender).BackColor = Color.FromArgb(19, 48, 78);
+        }
+
+        private void Menu_MouseHover(object sender, EventArgs e)
+        {
+            ((Label)sender).BackColor = Color.FromArgb(36, 68, 106);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
